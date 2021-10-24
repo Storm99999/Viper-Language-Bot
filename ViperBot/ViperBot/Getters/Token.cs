9948f7ded37bf30bd156f9e7b538b068aec3cdab
Storm <87811650+Storm99999@users.noreply.github.com>
2021-10-24 17:17:39 +0200

    Update Token.cs

--- ViperBot/ViperBot/Getters/Token.cs
+++ ViperBot/ViperBot/Getters/Token.cs
@@ -4,7 +4,7 @@
     {
         public static string GetToken()
         {
-            return "OTAwODA5NDc4ODIyMzI2MzM0.YXGuMw.ye13WuzG4PzGqza-XPX1y7YpjS4";
+            return "";
         }
     }
 }
